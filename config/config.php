486de07f7361f0a1e0b61796ee6924e810bb24c5
Asen Ivanov <a.ivanov89@mail.bg>
2018-08-19 02:13:37 +0300

    SQL Blog Site

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('ROOT_URL', 'http://localhost/phpcourse/websiteSQL/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'power');
+    define('DB_NAME', 'phpblog');
+
+?>
